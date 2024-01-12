44e2ad62492c6c6277349cfd503a1a0bb7146dd9
sonu bariki <70208080+sonubariki00@users.noreply.github.com>
2024-01-12 21:25:25 +0530

    Chalo Ayodhya
    23 ko jayenge

--- my-python-app/app.py
+++ my-python-app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World My name =HariOMM'
+    return 'Jay Siya Ram'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
